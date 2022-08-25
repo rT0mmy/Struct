3f22f61be7b6c0b8e3acae66067d336619890ed1
rT0mmy <46384146+rT0mmy@users.noreply.github.com>
2022-08-25 03:31:43 +0200

    Update Struct.lua

--- Struct.lua
+++ Struct.lua
@@ -1,1 +1,53 @@
+--!strict
+local Event = require(script.Parent:WaitForChild('event'))
 
+local Structs = {}
+Structs.__index = Structs
+
+export type StructObject = typeof(setmetatable({} :: {
+	Properties: {[string]: any},
+	Name: string,
+
+	onChanged: Event.Event,
+
+	Deconstruct:()->nil,
+	Copy:()->nil
+}, Structs))
+
+local function new(name:string):({[string]:any})->StructObject
+	return function(properties:{[string]:any}):StructObject
+		local self:{[string]:any} = {
+			Items = {},
+			Name = name,
+
+			onChanged = Event "onChanged",
+		}
+
+		function self:Copy()
+			return new(self.Name)(self.Properties)
+		end
+
+		function self:Deconstruct()
+			return
+		end
+
+		for i:string, v:any in properties do
+			self.Properties[i] = v
+		end
+
+		local StructObject = setmetatable({} :: any,{
+			__index = function(_,k)
+				return rawget(self,k) or rawget(self.Properties,k)
+			end,
+
+			__newindex = function(_,k,v)
+				self.Properties[k] = v
+				self.onChanged:Fire(k,v)
+			end,
+		})
+
+		return StructObject
+	end
+end
+
+return new
